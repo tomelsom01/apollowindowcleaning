71dde9a759739e0be2cebe4cba10dd2b1d9c7ac0
tomelsom <te28@hotmail.com>
2024-10-03 10:46:22 +0100

    cleaned up nav

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,27 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["toggleableElement", "blurBackground"]
+  static targets = ["toggalableElement"];
 
-  fire(event) {
-    const toggleableElement = this.toggleableElementTarget;
-    const blurBackground = this.blurBackgroundTarget;
-    const button = event.currentTarget;
-    const expanded = button.getAttribute('aria-expanded') === 'true';
-
-    // Toggle ARIA expanded attribute
-    button.setAttribute('aria-expanded', !expanded);
-
-    // Toggle menu visibility classes
-    toggleableElement.classList.toggle('menu-hidden');
-    toggleableElement.classList.toggle('menu-visible');
-
-    // Toggle background blur effect
-    blurBackground.classList.toggle('blurred-background-visible');
-
-    // Hide the menu button when the menu is open
-    this.menuButtonTarget.classList.toggle('menu-button-hidden');
-    this.closeButtonTarget.classList.toggle('menu-button-hidden');
+  toggle() {
+    // Toggle visibility classes for the sliding effect
+    this.toggalableElementTarget.classList.toggle("sidebar-visible");
+    this.toggalableElementTarget.classList.toggle("sidebar-hidden");
   }
 }
